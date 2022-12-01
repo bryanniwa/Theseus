c1df58a8cecb55da1aadc4d2529575f973ecf1c1
Linden Kyaw <kyawlinden@gmail.com>
2022-11-30 17:30:12 -0800

    Update task_async to contain ID

--- kernel/task_async/src/lib.rs
+++ kernel/task_async/src/lib.rs
@@ -3,17 +3,23 @@
 extern crate alloc;
 
 use alloc::boxed::Box;
-use core::task::{Context, Poll};
-use core::{future::Future, pin::Pin};
+use core::{
+    future::Future,
+    pin::Pin,
+    sync::atomic::{AtomicU64, Ordering},
+    task::{Context, Poll},
+};
 
 pub struct TaskAsync {
+    id: TaskId,
     future: Pin<Box<dyn Future<Output = ()>>>,
 }
 
 impl TaskAsync {
     pub fn new(future: impl Future<Output = ()> + 'static) -> TaskAsync {
         TaskAsync {
-            future: Box::pin(future),
+            id: TaskId::new(),
+            future:Box::pin(future),
         }
     }
 
@@ -21,5 +27,15 @@ impl TaskAsync {
     // I had to make it public so it could be accessed from the simple executor
     pub fn poll(&mut self, context: &mut Context) -> Poll<()> {
         self.future.as_mut().poll(context)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct TaskId(u64);
+
+impl TaskId {
+    fn new() -> Self {
+        static NEXT_ID: AtomicU64 = AtomicU64::new(0);
+        TaskId(NEXT_ID.fetch_add(1, Ordering::Relaxed))
     }
 }
